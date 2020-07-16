88196bcd61758aee37796d1f18faa2d4dbb39532
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-07-16 16:23:37 +0200

    Run subset of product tests against env with data protection

--- presto-product-tests/bin/product-tests-suite-3.sh
+++ presto-product-tests/bin/product-tests-suite-3.sh
@@ -22,5 +22,10 @@ presto-product-tests-launcher/bin/run-launcher test run \
     -- -g storage_formats,cli,hdfs_impersonation,authorization -x iceberg,"${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
+presto-product-tests-launcher/bin/run-launcher test run \
+    --environment singlenode-kerberos-hdfs-impersonation-with-data-protection \
+    -- -g storage_formats,cli,hdfs_impersonation,authorization -x iceberg,"${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
+    || suite_exit_code=1
+
 echo "$0: exiting with ${suite_exit_code}"
 exit "${suite_exit_code}"
